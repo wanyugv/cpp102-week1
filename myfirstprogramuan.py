d846c139f78f8dc0b0c4a8ba5bee1045475079a8
wanyugv <uanvllnva2@gmail.com>
2026-06-25 14:18:57 +0800

    Add files via upload

--- myfirstprogramuan.py
+++ myfirstprogramuan.py
@@ -1,4 +1,4 @@
 print("Hello, World")
 print("My name is Uan Gabriel D. Villanueva")
 print("My section is 1CPE-B")
-print("Today is June 22,2026 and itis currently 2:51")
+print("I am learning Python, Git, and Github")
